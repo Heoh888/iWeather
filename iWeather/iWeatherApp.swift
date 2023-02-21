d749282bdfcac961964e06e8b62dd7aee908b436
heoh888 <a.khodakow@gmail.com>
2023-02-21 15:06:26 +0300

    Initial Commit

---
+++ iWeather/iWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWeatherApp.swift
+//  iWeather
+//
+//  Created by Алексей Ходаков on 21.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct iWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
